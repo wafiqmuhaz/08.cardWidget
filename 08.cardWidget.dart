de8872a8846c0818a8e13db3c052e6aaa7d36800
Wafiq Muhammad Abdul Aziz <44719192+wafiqmuhaz@users.noreply.github.com>
2021-08-25 10:39:56 +0700

    Add files via upload

---
+++ 08.cardWidget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(allone());
+}
+
+class allone extends StatelessWidget {
+  const allone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.green,
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: ListView(
+            children: <Widget>[
+              buildCard(Icons.mail, "E-Mail"),
+              buildCard(Icons.map_sharp, "Maps"),
+              buildCard(Icons.lock_clock, "Clock"),
+              buildCard(Icons.alarm, "Alarm"),
+              buildCard(Icons.account_box, "Account"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String text) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: <Widget>[
+          Container(margin: EdgeInsets.all(5), child: Icon(Icons.mail)),
+          Text(text),
+        ],
+      ),
+    );
+  }
+}
